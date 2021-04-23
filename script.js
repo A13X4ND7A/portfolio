58f391601f13b10ab26c56f3dbec43e511fa243d
Alexandra <alex@faller.us>
2021-04-23 16:50:22 +0100

    working before i screw up

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-//allows the navbar to change colour when scrolling
-// $(window).scroll(function () {
-// 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-// });
+// // const typingTextDiv = document.getElementsByClassName('.typing');
+
+// const spanDiv = document.createElement('span');
+// const div = document.createElement('div');
+// const div2 = document.createElement('div');
+
+var str = "I am a <span class='wd-txt'>Web Developer</span>",
+	i = 0,
+	isTag,
+	text;
+
+(function type() {
+	text = str.slice(0, ++i);
+	if (text === str) return;
+
+	document.getElementById('typing').innerHTML = text;
+
+	var char = text.slice(-1);
+	if (char === '<') isTag = true;
+	if (char === '>') isTag = false;
+
+	if (isTag) return type();
+	setTimeout(type, 80);
+})();
+
+var stringToBeTyped = "k am not <span class='wd-txt'>Web Developer</span>",
+	x = 0,
+	isTags,
+	text2;
+
+function type2() {
+	text2 = stringToBeTyped.slice(0, ++x);
+	if (text2 === stringToBeTyped) return;
+
+	document.getElementById('typing2').innerHTML = text2;
+
+	var char = text2.slice(-1);
+	if (char === '<') isTags = true;
+	if (char === '>') isTags = false;
+
+	if (isTags) return type2();
+	setTimeout(type2, 80);
+}
+type2();
